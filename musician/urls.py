da8b91d892ae4dfe34be2e0e59f6e2e324845da2
Santiago Lamora <slamora@users.noreply.github.com>
2019-10-23 13:35:43 +0200

    Code login template.
    - Add base bootstrap files
    - Enable musician app

--- musician/urls.py
+++ musician/urls.py
@@ -4,11 +4,16 @@ URL routes definition.
 
 Describe the paths where the views are accesible.
 """
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
+from . import views
+
 
 app_name = 'musician'
 
 urlpatterns = [
-    # path('auth/login/', views.LoginView.as_view(), name='login'),
+    path('auth/login/', auth_views.LoginView.as_view(template_name='auth/login.html',
+                                                     extra_context={'version': '0.1'}), name='login'),
     # path('auth/logout/', views.LogoutView.as_view(), name='logout'),
 ]
