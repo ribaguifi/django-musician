ribaguifi/django-musician

commit 669600f4da2b46645064a0a5cb50aa93680096b6

Show user.last_login on dashboard

musician/models.py
@@ -1,6 +1,7 @@
 import ast
 import logging
 
+from django.utils.dateparse import parse_datetime
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
@@ -100,6 +101,7 @@ class UserAccount(OrchestraModel):
         'short_name': None,
         'full_name': None,
         'billing': {},
+        'last_login': None,
     }
 
     @classmethod
@@ -108,7 +110,10 @@ class UserAccount(OrchestraModel):
 
         if 'billcontact' in data:
             billing = BillingContact.new_from_json(data['billcontact'])
-        return super().new_from_json(data=data, billing=billing)
+
+        if 'last_login' in data:
+            last_login = parse_datetime(data['last_login'])
+        return super().new_from_json(data=data, billing=billing, last_login=last_login)
 
 
 class DatabaseUser(OrchestraModel):

musician/templates/musician/dashboard.html
@@ -4,7 +4,7 @@
 {% block content %}
 
 <h2>{% trans "Welcome back" %} <strong>{{ profile.username }}</strong></h2>
-<p>{% blocktrans with last_login=profile.last_login|default:"N/A" %}Last time you logged in was: {{ last_login }}{% endblocktrans %}</p>
+<p>{% blocktrans with last_login=profile.last_login|date:"SHORT_DATE_FORMAT" %}Last time you logged in was: {{ last_login }}{% endblocktrans %}</p>
 
 <div class="card-deck">
   {% for resource, usage in resource_usage.items %}

commit 160e0b059f141d48f85f6af915aabf401a06d55b

Bugfix: handle users that has never logged into
Regression introduced by 669600f4da2b46645064a0a5cb50aa93680096b6

musician/models.py
@@ -107,6 +107,7 @@ class UserAccount(OrchestraModel):
     @classmethod
     def new_from_json(cls, data, **kwargs):
         billing = None
+        last_login = None
 
         if 'billcontact' in data:
             billing = BillingContact.new_from_json(data['billcontact'])

musician/templates/musician/dashboard.html
@@ -4,7 +4,11 @@
 {% block content %}
 
 <h2>{% trans "Welcome back" %} <strong>{{ profile.username }}</strong></h2>
+{% if profile.last_login %}
 <p>{% blocktrans with last_login=profile.last_login|date:"SHORT_DATE_FORMAT" %}Last time you logged in was: {{ last_login }}{% endblocktrans %}</p>
+{% else %}
+<p>{% trans "It's the first time you log into the system, welcome on board!" %}</p>
+{% endif %}
 
 <div class="card-deck">
   {% for resource, usage in resource_usage.items %}

musician/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from .models import UserAccount
+
 
 class DomainsTestCase(TestCase):
     def test_domain_not_found(self):
@@ -12,3 +14,26 @@ class DomainsTestCase(TestCase):
 
         response = self.client.get('/domains/3/')
         self.assertEqual(404, response.status_code)
+
+
+class UserAccountTest(TestCase):
+    def test_user_never_logged(self):
+        data = {
+            'billcontact': {'address': 'foo',
+                            'city': 'Barcelona',
+                            'country': 'ES',
+                            'name': '',
+                            'vat': '12345678Z',
+                            'zipcode': '08080'},
+            'date_joined': '2020-01-14T12:38:31.684495Z',
+            'full_name': 'Pep',
+            'id': 2,
+            'is_active': True,
+            'language': 'EN',
+            'short_name': '',
+            'type': 'INDIVIDUAL',
+            'url': 'http://example.org/api/accounts/2/',
+            'username': 'pepe'
+        }
+        account = UserAccount.new_from_json(data)
+        self.assertIsNone(account.last_login)